cdf95278701da4e5f7962793243293858ec74614
Dufainder Bedoya <dufainder@gmail.com>
2022-04-11 14:28:59 -0500

    Agregando los modelos

--- api/src/models/Book.js
+++ api/src/models/Book.js
@@ -3,10 +3,43 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
-    name: {
+  sequelize.define('book', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    
+    title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    
+    state: {
+      type: DataTypes.BOOLEAN,
+    },
+
+    content_short: {
+      type: DataTypes.TEXT,
+    },
+
+    publisher_date:{
+      type: DataTypes.STRING,
+    },
+
+    image: {
+      type: DataTypes.STRING,
+    },
+
+    id_api:{
+      type: DataTypes.FLOAT,
+    },
+
   });
 };
